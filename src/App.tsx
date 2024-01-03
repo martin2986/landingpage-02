377459434f9ca6132bfe16e0fb3b7894abc1007d
martins2986 <m.devit@pm.me>
2024-01-03 20:39:32 +0100

    app setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" text-center text-3xl mt-5 font-medium">Landing Page</h1>
+    </div>
+  );
+}
+
+export default App;

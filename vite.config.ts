6a6dedcfbf8e76b2defa483a112f31bc235d3f20
martins2986 <m.devit@pm.me>
2024-05-18 15:06:10 +0200

    modified client dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-export default defineConfig({
-  plugins: [react(), eslint()],
-});
+
+import { defineConfig, loadEnv } from "vite";
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const url = env.VITE_BACKEND_SERVER || "http://localhost:8080";
+
+  const config = {
+    plugins: [react(), eslint()],
+    define: {
+      "process.env": env,
+    },
+    resolve: {
+      base: "/",
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: url,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+  return defineConfig(config);
+};
